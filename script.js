0fdcb41bcc00a4e5d05e1d5e865b38143f611ecd
Edilawit Tsehay <etsehay@internal.salesforce.com>
2022-06-21 06:31:04 -0700

    final commit

--- script.js
+++ script.js
@@ -1,102 +1,79 @@
-const apiKey = "4bbb7e45de73a2addf2cd9b4eabc0ac5";
-const posterPath = "https://www.themoviedb.org/t/p/original"; 
+const apiKey = "4bbb7e45de73a2addf2cd9b4eabc0ac5"
 
 //DOM selectors
 
-const loadMoreMoviesBtn = document.querySelector("#load-more-movies-btn")
-const searchEl = document.querySelector("#submit")
-const clearSearchBtn = document.querySelector("#close-search-btn")
-const movieGrid = document.querySelector(".movie-grid");
-const movieTitle = document.querySelector(".movie-title");
-const moviePoster = document.querySelector("#movie-poster");
-const movieVotes = document.querySelector(".movie-votes");
+const loadMoreBtnEl = document.querySelector("#load-more-movies-btn");
+//const formEl = document.querySelector("form");
+const closeSearchBtnEl = document.querySelector("#close-search-btn");
+const searchInputEl = document.querySelector("#search-input");
+const searchEl = document.querySelector("#search")
+const movieGridEl = document.querySelector(".movie-grid");
+const resultsEl = document.querySelector("#searchResults");
+
 
 //parameters
 
-let page = 1
-let search = false
-let searchKey = ""
-let showMore = false
-nowPlaying = false
+let page = 1;
+let searchKey = "";
+let starter = false;
 
 //functions
 
-async function recommendedMovies() { 
-    try { 
-        nowPlaying = true
-        search = false
-        
-     // let url = "https://api.themoviedb.org/3/search/movie/now_playing?api_key=" + apiKey + "&language=en-US&page=" + page
-      let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=${page}`
-      page++;
-      
-        let response = await fetch(url); 
-        let resData = await response.json(); 
-    
-         displayMovies(resData);
-   }
-    catch(err) {
-        console.log("Error");
-    }
+function nowPlayingMovies(searchKey, noInput ) { 
+    if(noInput){
+            return `https://api.themoviedb.org/3/movie/now_playing?api_key="${apiKey}"&language=en-US&page=${page}`;
+         }
+    else{
+            return `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchKey}&language=en-US&page=${page}`;
+        }
+
 }
 
 
-function displayMovies(data) {
-      
-        data.results.forEach((item) =>{
-            if(item.Poster_Path == null){
-             movieGrid.innerHTML +=  `
-             <span class="movie-card">
-                 <img id="movie-poster" src="placeholder.png" alt = ${item.title}>
-                 <div class="movie-title">${item.title}</div>
-                 <div class="movie-date">${item.release_date}</div>
-                 <span div="movie-votes" id = ${item.id}>⭐${item.vote_average}</span>
-            </span>
-             <div class="space">
-             </div>`        
-    }
-            else{
-                movieGrid.innerHTML +=  `
-             <span class="movie-card">
-                 <img id="movie-poster" src=${posterPath + item.poster_path} alt = ${item.title}>
-                 <div class="movie-title">${item.title}</div>
-                 <div class="movie-date">${item.release_date}</div>
-                 <span div="movie-votes" id = ${item.id}>⭐${item.vote_average}</span>
-            </span>
-             <div class="space">
-             </div>`  
-            }
-    })
-     }
-     
+async function getResults() {
 
-async function getResults(ev) {
+    ev.preventDefault();
+    starter = false;
+    searchKey = userInput.value;
 
-    ev.preventDefault()
-    search  = true
-    nowPlaying = false
-    searchKey = userInput.value
-    if (ev.target.className == "search-input"){
-        url = `https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&page=" + page + "&query=" + searchKey`
+    if (ev.target.className == searchKey){
+        url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchKey}&language=en-US&page=${page}`
     }
     else{
-        url = `https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&page=" + page`
+        url = `https://api.themoviedb.org/3/movie/now_playing?api_key="${apiKey}"&language=en-US&page=${page}`
     }
 
 
-let response = await fetch(url); 
+        let response = await fetch(url); 
+        console.log("response is: ",response)
         let resData = await response.json(); 
+        console.log("resData is: ",resData)
+        displayMovies(resData);
+        return resData;
     
-         displayMovies(resData);
-         page ++;
    }
+
+   function displayMovies(responseData) {
+      
+    responseData.results.forEach(item =>{
+        movieGridEl.innerHTML +=  `
+             <span class="movie-card">
+             <img id="movie-poster" src="https://image.tmdb.org/t/p/original/${item.poster_path}" alt = ${item.title} class = "movie-poster">
+             <div class="movie-title">${item.title}</div>
+             <div class="movie-date">${item.release_date}</div>
+             <span div="movie-votes" id = ${item.id}>⭐${item.vote_average}</span>
+        </span>
+         <div class="space">
+         </div>`              
+})
+}
+ 
 
 
 function clearSearch(){
-    movieGrid.innerHTML= ``
-    loadMore = false
-    //..
-    recommendedMovies();
+    movieGridEl.innerHTML= ``
+    loadMore = false;
+    nowPlayingMovies();
 }
 
 
@@ -104,27 +81,29 @@ function clearSearch(){
 
 searchEl.addEventListener("submit", (event) =>{
     event.preventDefault()
-    if (event.target.searchTerm.value == searchKey){
+    starter = false;
+    searchEl.value = "";
+        if (event.target.searchTerm.value == searchKey){
         getResults(searchKey);
     }
     else{
-        recommendedMovies();
+        nowPlayingMovies();
     }
 })
 
-loadMoreMoviesBtn.addEventListener("click", (event) =>{
+loadMoreBtnEl.addEventListener("click", (event) =>{
     if (searchKey){
         getResults(searchKey);
     }
     else{
-        recommendedMovies();
+        nowPlayingMovies();
     }
 })
  
 
-clearSearchBtn.addEventListener("click", clearSearch)
+closeSearchBtnEl.addEventListener("click", clearSearch)
 
 window.onload = () => {
-     result = true;
-     recommendedMovies();
+     starter = true;
+     nowPlayingMovies();
  }
