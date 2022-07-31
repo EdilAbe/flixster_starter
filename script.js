9b3fef32f332aee61abac733d9f18c009e6b1d5b
Edilawit Tsehay <edilawittsehay@gmail.com>
2022-07-30 20:09:41 -0700

    added gif

--- script.js
+++ script.js
@@ -1,109 +1,117 @@
-const apiKey = "4bbb7e45de73a2addf2cd9b4eabc0ac5"
+const API_KEY = "4bbb7e45de73a2addf2cd9b4eabc0ac5";
+BASE_API_URL = "https://api.themoviedb.org/3";
 
 //DOM selectors
-
-const loadMoreBtnEl = document.querySelector("#load-more-movies-btn");
+const loadMoreBtn = document.querySelector("#load-more-movies-btn");
 //const formEl = document.querySelector("form");
-const closeSearchBtnEl = document.querySelector("#close-search-btn");
-const searchInputEl = document.querySelector("#search-input");
-const searchEl = document.querySelector("#search")
-const movieGridEl = document.querySelector(".movie-grid");
-const resultsEl = document.querySelector("#searchResults");
-
+const closeSearchBtn = document.querySelector("#close-search-btn");
+const searchBtn = document.querySelector("#searchButton");
+const moviesGrid = document.querySelector("#movie-grid");
+const moviesCard = document.querySelector("#movie-card");
+//const resultsEl = document.querySelector("#searchResults");
+const popup = document.querySelector("#popup");
+const popupContainer = document.querySelector("#popup-container");
 
 //parameters
 
 let page = 1;
-let searchKey = "";
-let starter = false;
+let searchTerm = "";
+let latestApiCall = "";
+let searched = false;
 
 //functions
+async function callAPI(apiRequestURL) {
+  console.log("called URL:", apiRequestURL);
 
-function nowPlayingMovies(searchKey, noInput ) { 
-    if(noInput){
-            return `https://api.themoviedb.org/3/movie/now_playing?api_key="${apiKey}"&language=en-US&page=${page}`;
-         }
-    else{
-            return `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchKey}&language=en-US&page=${page}`;
-        }
-
+  let response = await fetch(apiRequestURL);
+  let responseData = await response.json();
+  return responseData;
 }
 
+async function getResults(apiRequestURL) {
+  latestApiCall = apiRequestURL;
+  console.log("latest call", latestApiCall);
 
-async function getResults() {
+  results = await callAPI(apiRequestURL);
+  return results;
+}
 
-    ev.preventDefault();
-    starter = false;
-    searchKey = userInput.value;
-
-    if (ev.target.className == searchKey){
-        url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchKey}&language=en-US&page=${page}`
-    }
-    else{
-        url = `https://api.themoviedb.org/3/movie/now_playing?api_key="${apiKey}"&language=en-US&page=${page}`
-    }
-
-
-        let response = await fetch(url); 
-        console.log("response is: ",response)
-        let resData = await response.json(); 
-        console.log("resData is: ",resData)
-        displayMovies(resData);
-        return resData;
-    
-   }
-
-   function displayMovies(responseData) {
-      
-    responseData.results.forEach(item =>{
-        movieGridEl.innerHTML +=  `
-             <span class="movie-card">
+function displayMovies(responseData) {
+  responseData.results.forEach((item) => {
+    moviesCard.innerHTML += `
+             <span class="movie-card-each">
              <img id="movie-poster" src="https://image.tmdb.org/t/p/original/${item.poster_path}" alt = ${item.title} class = "movie-poster">
              <div class="movie-title">${item.title}</div>
              <div class="movie-date">${item.release_date}</div>
              <span div="movie-votes" id = ${item.id}>⭐${item.vote_average}</span>
         </span>
-         <div class="space">
-         </div>`              
-})
-}
- 
-
-
-function clearSearch(){
-    movieGridEl.innerHTML= ``
-    loadMore = false;
-    nowPlayingMovies();
+         `;
+  });
 }
 
+async function nowPlayingMovies() {
+  movieResults = await getResults(
+    BASE_API_URL + "/movie/now_playing?api_key=" + API_KEY
+  );
+  displayMovies(movieResults);
+}
 
- //event listeners
+function clearSearch() {
+  moviesGrid.innerHTML = ``;
+  loadMore = false;
+  nowPlayingMovies();
+}
 
-searchEl.addEventListener("submit", (event) =>{
-    event.preventDefault()
-    starter = false;
-    searchEl.value = "";
-        if (event.target.searchTerm.value == searchKey){
-        getResults(searchKey);
-    }
-    else{
-        nowPlayingMovies();
-    }
-})
+//event listeners
 
-loadMoreBtnEl.addEventListener("click", (event) =>{
-    if (searchKey){
-        getResults(searchKey);
-    }
-    else{
-        nowPlayingMovies();
-    }
-})
- 
+searchBtn.addEventListener("click", async (event) => {
+  event.preventDefault();
+  page = 1;
+  // moviesGrid.innerHTML = "";
 
-closeSearchBtnEl.addEventListener("click", clearSearch)
+  searchTerm = document.getElementById("search-input").value;
+
+  let resp = await fetch(
+    BASE_API_URL +
+      "/search/movie?api_key=" +
+      API_KEY +
+      "&query=" +
+      searchTerm +
+      "&page=" +
+      page
+  );
+  let result = await resp.json();
+  //  searched = True;
+  console.log(" searchTerm: ", searchTerm);
+
+  console.log("result : ", result);
+
+  displayMovies(result);
+  // closeSearchBtn.classList.remove("hidden");
+});
+
+loadMoreBtn.addEventListener("click", async () => {
+  page += 1;
+  results = await getResults(
+    BASE_API_URL + "/movie/now_playing?api_key=" + API_KEY + "&page=" + page
+  );
+
+  displayMovies(results);
+});
+
+closeSearchBtn.addEventListener("click", () => {
+  moviesGrid.innerHTML = "";
+  page = 1;
+  nowPlayingMovies();
+  closeSearchBtn.classList.add("hidden");
+});
+
+popupContainer.addEventListener("click", () => {
+  popupContainer.classList.add("hidden");
+  popup.classList.add("hidden");
+});
 
 window.onload = () => {
-     starter = true;
-     nowPlayingMovies();
- }
+  page = 1;
+  nowPlayingMovies();
+};
